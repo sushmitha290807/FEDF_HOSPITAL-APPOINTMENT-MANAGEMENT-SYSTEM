d5ffb9c6b32bdc78c0e43f9aeb4f48b351dcc2de
sushmitha290807 <sushmithapoojari2907@gmail.com>
2026-06-03 08:25:53 +0530

    Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header
+      style={{
+        backgroundColor: "#1976d2",
+        color: "white",
+        padding: "15px",
+        textAlign: "center",
+      }}
+    >
+      <h1>Doctor Appointment Booking System</h1>
+    </header>
+  );
+}
+
+export default Header;
