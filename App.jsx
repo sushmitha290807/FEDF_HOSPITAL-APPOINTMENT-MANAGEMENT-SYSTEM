5547f27234f18cbe03c7d2500b5dedeb7c4323b4
sushmitha290807 <sushmithapoojari2907@gmail.com>
2026-06-03 08:28:51 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Login />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
