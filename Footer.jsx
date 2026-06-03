5aa4bb3e4fa551352bbe67d3c99a8a3df9f2360b
sushmitha290807 <sushmithapoojari2907@gmail.com>
2026-06-03 08:26:43 +0530

    Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#1976d2",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+        marginTop: "20px",
+      }}
+    >
+      <p>© 2026 Doctor Appointment System</p>
+    </footer>
+  );
+}
+
+export default Footer;
